5396caee8b3cabf3bb074a0d5c6ce1bada7d26d0
khawar-saleem-qamar <khawarsaleemqamar@gmail.com>
2024-08-21 20:09:01 +0500

    khwar

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config()
 
+console.log("url for mongodb: ", process.env.MONGO_URL)
 const connection = mongoose.createConnection(process.env.MONGO_URL)
     .on('open',()=>{
     console.log('MongoDb connected');
