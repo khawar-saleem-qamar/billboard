04c447763cd391b3795fe8c0ad035354866dc310
khawar-saleem-qamar <khawarsaleemqamar@gmail.com>
2024-08-21 20:07:49 +0500

    alsdkfja

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const connection = mongoose.createConnection(process.env.MONGO_URL)
     .on('open',()=>{
     console.log('MongoDb connected');
-}).on('error',()=>{
+}).on('error',(error)=>{
+    console.log(error)
     console.log('MongoDb error');
 });
 
